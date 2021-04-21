be480e624f2c292efd02f77abf5d97ad7f83f09f
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-04-21 03:48:45 +0200

    write insert table queries

--- sql_queries.py
+++ sql_queries.py
@@ -20,16 +20,15 @@ time_table_create = ("""CREATE TABLE IF NOT EXISTS time(start_time timestamp PRI
 
 # INSERT RECORDS
 
-songplay_table_insert = (""" """)
+songplay_table_insert = ("""INSERT INTO songplays(songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (songplay_id) DO NOTHING;""")
 
-user_table_insert = (""" """)
+user_table_insert = ("""INSERT INTO users(user_id, first_name, last_name, gender, level) VALUES(%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING;""")
 
-song_table_insert = (""" """)
+song_table_insert = ("""INSERT INTO songs(song_id, title, artist_id, year, duration) VALUES(%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING;""")
 
-artist_table_insert = (""" """)
+artist_table_insert = ("""INSERT INTO artists(artist_id, name, location, latitude, longitude) VALUES(%s, %s, %s, %s, %s) ON CONFLICT (artist_id) DO NOTHING;""")
 
-
-time_table_insert = (""" """)
+time_table_insert = ("""INSERT INTO time(start_time, hour, day, week, month, year, weekday) VALUES(%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING;""")
 
 # FIND SONGS
 
