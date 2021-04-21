3aab795ec5f90076d31a846a650ba7cf81a818b6
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-04-21 03:52:14 +0200

    implement etl.process_song_file

--- etl.py
+++ etl.py
@@ -6,7 +6,17 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    pass
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # insert song record
+    d = df.to_dict('records')[0]
+    song_data = [d["song_id"], d["title"], d["artist_id"], d["year"], d["duration"]]
+    cur.execute(song_table_insert, song_data)
+
+    # insert artist record
+    artist_data = [d["artist_id"], d["artist_name"], d["artist_location"], d["artist_latitude"], d["artist_longitude"]]
+    cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
