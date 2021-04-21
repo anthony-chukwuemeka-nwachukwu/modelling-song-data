7fe77ca7d033610109dd2744cc383c124ce4157e
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-04-21 03:55:03 +0200

    implement etl.process_data

--- etl.py
+++ etl.py
@@ -54,7 +54,22 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
-    pass
+    # get all files matching extension from directory
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, '*.json'))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    # get total number of files found
+    num_files = len(all_files)
+    print('{} files found in {}'.format(num_files, filepath))
+
+    # iterate over files and process
+    for i, datafile in enumerate(all_files, 1):
+        func(cur, datafile)
+        conn.commit()
+        print('{}/{} files processed.'.format(i, num_files))
 
 
 def main():
