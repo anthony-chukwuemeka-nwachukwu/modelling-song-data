fa3a0746200c7ab67d0659172d16c12c403076eb
Nwachukwu Anthony <nwachukwuanthony2015@gmail.com>
2021-04-21 03:31:53 +0200

    implement create_tables.create_database

--- create_tables.py
+++ create_tables.py
@@ -7,7 +7,23 @@ def create_database():
     - Creates and connects to the sparkifydb
     - Returns the connection and cursor to sparkifydb
     """
-    pass
+    # connect to default database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=studentdb user=student password=student")
+    conn.set_session(autocommit=True)
+    cur = conn.cursor()
+
+    # create sparkify database with UTF8 encoding
+    cur.execute("DROP DATABASE IF EXISTS sparkifydb")
+    cur.execute("CREATE DATABASE sparkifydb WITH ENCODING 'utf8' TEMPLATE template0")
+
+    # close connection to default database
+    conn.close()
+
+    # connect to sparkify database
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    cur = conn.cursor()
+
+    return cur, conn
 
 
 def drop_tables(cur, conn):
